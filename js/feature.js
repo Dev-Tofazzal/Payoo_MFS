3aad7faae947fa3b4cb4ec0e1a9644ab1ae702bb
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-03-10 12:04:54 +0600

    feature correction

---
+++ js/feature.js
@@ -0,0 +1,8 @@
+document.getElementById("show_cashOut").addEventListener("click",function(){
+    document.getElementById("cash_out_form").classList.remove('hidden')
+    document.getElementById("add_money_form").classList.add('hidden')
+})
+document.getElementById("show_addMoney").addEventListener("click",function(){
+    document.getElementById("add_money_form").classList.remove('hidden')
+    document.getElementById("cash_out_form").classList.add('hidden')
+})
