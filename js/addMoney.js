3ec84014b44452faa8dc72ea9e71e88320ce3518
mdtofazzalislam <dev.tofazzal122@gmail.com>
2025-03-10 11:53:36 +0600

    window_new_page added

--- js/addMoney.js
+++ js/addMoney.js
@@ -3,13 +3,13 @@ document.getElementById("btn_add_money")
     .addEventListener("click",function(e){
         e.preventDefault();
         const addMoney = document.getElementById("input_add_money").value ;
+        const addMoneyNumber = parseFloat(addMoney)
         
         const pinNumber = document.getElementById("input_pin_number").value;
         console.log(addMoney,pinNumber);
 
         if(pinNumber === "1234"){
             const balance = document.getElementById("balance").innerText;
-            const addMoneyNumber = parseFloat(addMoney)
             const balanceNumber = parseFloat(balance)
             
             
--- js/cashOut.js
+++ js/cashOut.js
@@ -1,11 +1,11 @@
 document.getElementById("cash_out_btn").addEventListener("click",function(e){
     e.preventDefault();
     const cashOut = document.getElementById("cashOut_money").value;
+    const cashOutValue = parseFloat(cashOut)
     const cashOutPin = document.getElementById("cashOut_pin_number").value;
 
     if(cashOutPin === "1234"){
        const balance = document.getElementById("balance").innerText;
-       const cashOutValue = parseFloat(cashOut)
        const balanceValue = parseFloat(balance)
        const newBalance = balanceValue - cashOutValue;
        
